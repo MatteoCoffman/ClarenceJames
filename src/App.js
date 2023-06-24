28531ad3e91344b14c68214e5af298cae2972ed4
Matteo <matteo.coffman@gmail.com>
2023-06-24 03:32:30 -0500

    Added files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Listen from "./pages/Listen";
+import Tour from "./pages/Tour";
+import Shop from "./pages/Shop";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <p>Hiya!</p>;
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/listen" component={Listen} />
+          <Route path="/tour" component={Tour} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
