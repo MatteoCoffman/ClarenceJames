4c3f44d7368e876543bc85c2245712f6938c52e7
Matteo <matteo.coffman@gmail.com>
2023-06-26 12:43:05 -0500

    Added Spotify Players to Listen page

---
+++ src/components/Player.js
@@ -0,0 +1,14 @@
+export default function Player({ Uri, type }) {
+  return (
+    <div>
+      <iframe
+        src={`https://open.spotify.com/embed/${type}/${Uri}`}
+        width={300}
+        height={380}
+        frameBorder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+      ></iframe>
+    </div>
+  );
+}
--- src/pages/Listen.js
+++ src/pages/Listen.js
@@ -1,7 +1,32 @@
+import Player from "../components/Player";
+import "./Listen.css";
+
 export default function Listen() {
+  const albums = [{ uri: "2fjs85edQPLm8UFUzYNLfs", title: "Fucked Me Up" }];
+
+  const tracks = [
+    {
+      uri: "03bnnlRI981SeYlaSPsYTp?si=8b9db6040f074c09",
+      title: "I Think It Might Be Hell",
+    },
+  ];
+
   return (
     <div>
-      <h1>Listen</h1>
+      <div className="player-container">
+        {albums.map((album) => (
+          <div key={album.uri} className="player-item">
+            <h2>{album.title}</h2>
+            <Player Uri={album.uri} type="album" />
+          </div>
+        ))}
+        {tracks.map((track) => (
+          <div key={track.uri} className="player-item">
+            <h2>{track.title}</h2>
+            <Player Uri={track.uri} type="track" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
