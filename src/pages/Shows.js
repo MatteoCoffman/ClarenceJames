9afa9fbb63156c8562d417668ce57e97215fda0d
Matteo <matteo.coffman@gmail.com>
2023-07-13 14:30:46 -0500

    Added Stevie Link

--- src/pages/Shows.js
+++ src/pages/Shows.js
@@ -2,6 +2,10 @@ export default function Shows() {
   return (
     <div>
       <h1>No Shows Right Now :(</h1>
+      <p>
+        For booking contact{" "}
+        <a href="https://www.instagram.com/steviemayneee/">@steviemayneee</a>
+      </p>
     </div>
   );
 }
